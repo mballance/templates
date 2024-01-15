2f27707e484d23e3331947661fdde26db9f4b874
Matthew Ballance <matt.ballance@gmail.com>
2024-01-14 17:43:35 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- py/class/apache/copyright/class.py
+++ py/class/apache/copyright/class.py
@@ -3,7 +3,7 @@
 #****************************************************************************
 #* {{filename_base}}.py
 #*
-#* Copyright 2022 Matthew Ballance and Contributors
+#* Copyright 2023 Matthew Ballance and Contributors
 #*
 #* Licensed under the Apache License, Version 2.0 (the "License"); you may 
 #* not use this file except in compliance with the License.  
